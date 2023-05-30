601b4d8433bacf694e1f86c6d6cf62f9dc927aed
Bruno crepaldi <107934654+BrunoCrepaldi17@users.noreply.github.com>
2023-05-29 22:03:01 -0300

    Add files via upload

---
+++ App/Models/ItemVenda.php
@@ -0,0 +1,14 @@
+<?php
+ namespace App\Models;
+
+class ItemVenda{
+private $item;
+
+public function __get($atributo){
+return $this ->$atributo;
+}
+
+public function __set($atributo, $valor){
+    $this ->$atributo = $valor;
+    }
+}
